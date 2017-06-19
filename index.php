1fc3fe24500565d9ea35cf38f3cbadc696e60ed5
Daniel WIllson <daniel@danielwillson.me>
2017-06-19 08:02:20 -0400

    underscores add to develop

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+ 
+get_header(); ?>
+ 
+<!-- KATHERINE CODE BELOW -->
+
+
+<!-- FELIX CODE BELOW -->
+
+
+<!-- BRANDON CODE BELOW -->
+
+
+<!-- KATIE CODE BELOW -->
+
+
+<!-- JULIUS CODE BELOW -->
+
+
+<!-- CHRIS CODE BELOW -->
+
+
+
+
+    <?php get_template_part( 'loop', 'index' ); ?>
+ 
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
---
+++ page-program.php
@@ -0,0 +1,212 @@
+<?php
+/**
+ * Template Name: Program Page
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+
+get_header(); ?>
+
+	<?php //get_template_part( 'loop', 'page' ); ?>
+
+	<!-- JULIUS TOP CODE HERE -->
+	<div class="Title"><h1>Hustle</h1><h3>HackCville's Entrepreneurship Program</h3></div>
+
+<svg width="70" height="55" viewBox="-2.5 -5 75 60" preserveAspectRatio="none" >
+  <path d="M0,0 l35,50 l35,-50" fill="none" stroke="white" stroke-linecap="round" stroke-width="5" />
+</svg>
+
+	<!-- FELIX CODE HERE -->
+<div class="section-1">
+	<div class="responsive-container">
+		<div class="section-1-content">
+		<div class="left-col">
+			<h2 class="subject">About the Program</h2>
+			<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+
+			<div class="apply-now">
+				<p><a href="#"> APPLY NOW </a></p>
+			</div>
+			<div class="learn-more">
+				<p><a href="#"> LEARN MORE </a></p>
+			</div>
+
+		</div>
+
+		
+
+		<div class="right-col">
+			<h2 class="subject">What's Covered</h2>
+			<div class="timeline">
+				  <!-- Item 1 -->
+				    <div class="direction-r">
+				      <p>Lorem ipsum Nisi labore aute do aute culpa magna nulla voluptate exercitation deserunt proident.</p>
+				    </div>
+
+				    <div class="direction-l">
+				      <p>Lorem ipsum Nisi labore aute do aute culpa magna nulla voluptate exercitation deserunt proident.</p>
+				    </div>
+
+				    <div class="direction-r">
+				      <p>Lorem ipsum Nisi labore aute do aute culpa magna nulla voluptate exercitation deserunt proident.</p>
+				    </div>
+			</div>
+		</div>
+
+		</div>
+	</div>
+
+</div>
+
+	<!-- KATIE CODE HERE -->
+<h1>Meet the Program Leads</h1>
+<div class="top-left">
+	<img src="daniel.jpg"/>
+	<div class="top-left-text">
+
+		<div class="name-heading">
+		<h2>Daniel Willson</h2></div>
+
+	<p>This is Daniel he's cool and he plays the fiddle too and he will teach you cool things This is Daniel he's cool and he plays the fiddle too and he will teach you cool thingsThis is Daniel he's cool and he 
+	</p></div>
+
+</div>
+
+<div class="top-right">
+	<img src="daniel.jpg"/>
+	<div class="top-right-text">
+
+		<div class="name-heading">
+		<h2>Daniel Willson</h2></div>
+
+	<p>This is Daniel he's cool and he plays the fiddle too and he will teach you cool things This is Daniel he's cool and he plays the fiddle too and he will teach you cool thingsThis is Daniel he's cool and he 
+	</p></div>
+
+</div>
+
+<div class="bottom-left">
+	<img src="daniel.jpg"/>
+	<div class="bottom-left-text">
+
+		<div class="name-heading">
+		<h2>Daniel Willson</h2></div>
+
+	<p>This is Daniel he's cool and he plays the fiddle too and he will teach you cool things This is Daniel he's cool and he plays the fiddle too and he will teach you cool thingsThis is Daniel he's cool and he 
+	</p></div>
+
+</div>
+
+<div class="bottom-right">
+	<img src="daniel.jpg"/>
+	<div class="bottom-right-text">
+
+		<div class="name-heading">
+		<h2>Daniel Willson</h2></div>
+
+	<p>This is Daniel he's cool and he plays the fiddle too and he will teach you cool things This is Daniel he's cool and he plays the fiddle too and he will teach you cool thingsThis is Daniel he's cool and he 
+	</p></div>
+
+</div>
+
+	<!-- JULIUS CODE HERE -->
+		
+	<div class="instructor">
+
+		<div class="instructorphoto"><img src="dw-prof1.jpg" /></div>
+		<div class="blurb"><h4>Daniel Willson</h4><h5>Lorem Ipsum Dolororem Ipsum Dolororemrorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum Drorem Ipsum D Ipum Dolororem Ipsum Dolororem Ipsum Dolororem Ipsum Dolor Ipsum Dolororem Ipsum Dolor</h5></div> 
+
+	</div>
+
+
+	<!-- BRANDON CODE HERE -->
+
+	<div class="container">
+		<div class="title">
+			<h1>Student Outcomes</h1>
+		</div> 
+		<div class="content">
+			<div class="outcome">
+				<div class="image">
+					<img src="images/uva.jpg">
+				</div>
+				<div class="caption">
+					<h1>Title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+
+			<div class="outcome">
+				<div class="image">
+					<img src="images/uva.jpg">
+				</div>
+				<div class="caption">
+					<h1>Title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+				</div>
+			</div>
+
+			<div class="outcome">
+				<div class="image">
+					<img src="images/uva.jpg">					
+				</div>
+				<div class="caption">
+					<h1>Title</h1>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+	<!-- CHRIS CODE HERE -->
+<div class="bg">
+	</div>
+
+		<div class="title">
+			<h1>What's Included</h1>
+		</div>
+
+		<div class="column">
+			<img src="icon.png">
+				<div class="text-header">
+					<h3>HC Membership</h3>
+				</div>
+				<div class="text-paragraph">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat</p>
+				</div>
+		</div>
+
+		<div class="column">
+			<img src="icon.png">
+				<div class="text-header">
+					<h3>HC Membership</h3>
+				</div>
+				<div class="text-paragraph">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat</p>
+				</div>
+		</div>
+
+		<div class="column">
+			<img src="icon.png">
+				<div class="text-header">
+					<h3>HC Membership</h3>
+				</div>
+				<div class="text-paragraph">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat</p>
+				</div>
+		</div>
+
+		<div class="column">
+			<img src="icon.png">
+				<div class="text-header">
+					<h3>HC Membership</h3>
+				</div>
+				<div class="text-paragraph">
+					<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat</p>
+				</div>
+		</div>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
